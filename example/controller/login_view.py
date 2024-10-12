e5239e28a3e3947c85b02b4de0b00a5446261d11
afcarioca <afcarioca@hotmail.com>
2024-10-12 18:42:02 -0300

    Add Token

--- example/controller/login_view.py
+++ example/controller/login_view.py
@@ -1,7 +1,23 @@
-
 from rest_framework.views import APIView
+import json
+from django.http import HttpResponse
+from example.validator.login_form import LoginForm
+from django.http import JsonResponse
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class LoginView(APIView):
     def post(self, request):
-        print(request)
+        data = json.loads(request.body)
+        form = LoginForm(data)
+        if not form.is_valid():
+           errors = dict(form.errors.items())
+           return HttpResponse(JsonResponse({'status': 'Erro', 'message': 'Erros de Validação', 'errors': errors}),  content_type="application/json", status= 400) 
+        
+        user = authenticate(username=data["username"],password=data["senha"])
+        if user is None:
+                return HttpResponse(JsonResponse({'status': 'Acesso Negado', 'message':'Usuário Inexistente'}), content_type="application/json", status=403)
+        
+        refresh = RefreshToken.for_user(user)
+        return HttpResponse(JsonResponse({'status': 'OK', 'message':'Sucesso','Refresh':str(refresh),'Token':str(refresh.access_token)}), content_type="application/json", status=200)
--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,10 @@
 # example/urls.py
 from django.urls import path
-
 from example.views import index
+from example.controller.login_view import LoginView
 
 
 urlpatterns = [
     path('', index),
+    path("api/login", LoginView.as_view(), name="login")
 ]
