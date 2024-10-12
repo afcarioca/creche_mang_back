fb03cde19e6217672a571050a73388aeef87f620
afcarioca <afcarioca@hotmail.com>
2024-10-12 11:31:03 -0300

    Again

--- api/prod.py
+++ api/prod.py
@@ -7,7 +7,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
+##SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
