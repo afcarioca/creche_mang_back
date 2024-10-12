76d95e4dec0ddfd9a4c727e9c72ff0ea71e0e78f
afcarioca <afcarioca@hotmail.com>
2024-10-12 11:25:58 -0300

    Changing function of dot_env

--- api/prod.py
+++ api/prod.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
