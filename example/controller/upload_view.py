df2447557d027790bdf070d5311dfb12ba82ba7f
afcarioca <afcarioca@hotmail.com>
2024-10-12 22:51:59 -0300

    Add Upload

---
+++ example/controller/upload_view.py
@@ -0,0 +1,21 @@
+from rest_framework.views import APIView
+import json
+from django.http import HttpResponse
+from example.validator.login_form import LoginForm
+from django.http import JsonResponse
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import pandas as pd
+
+
+class UploadView(APIView):
+    def post(self, request):
+        file_obj = request.FILES["xlsx"]
+        df = pd.read_excel(file_obj)
+        print(df)
+       
+        return HttpResponse()
+        
+     
--- example/urls.py
+++ example/urls.py
@@ -2,11 +2,13 @@
 from django.urls import path
 from example.views import index
 from example.controller.login_view import LoginView
+from example.controller.upload_view import UploadView
 from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
     path('', index),
-    path("api/login/", LoginView.as_view(), name="login")
+    path("api/login/", LoginView.as_view(), name="login"),
+    path("api/upload/", UploadView.as_view(), name="upload")
 ]
 
