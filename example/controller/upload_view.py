1f33ed83ac7aa23ed226316e472c24197e186899
afcarioca <afcarioca@hotmail.com>
2024-10-14 00:01:24 -0300

    Add

--- example/controller/upload_view.py
+++ example/controller/upload_view.py
@@ -8,13 +8,45 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 import pandas as pd
-
+from django.core import serializers
 
 class UploadView(APIView):
+  
+
     def post(self, request):
         file_obj = request.FILES["xlsx"]
         df = pd.read_excel(file_obj)
-        print(df)
-        return HttpResponse(JsonResponse({'status': 'OK', 'message':'Sucesso'}), content_type="application/json", status=200)
+        df = df.drop(columns=["Professora", 'Carimbo de data/hora', "nome"]) 
+
+        df.replace("Berçário", "bercario", inplace=True)
+        df.replace("Maternal 1", "maternal1", inplace=True)
+        df.replace("Maternal 2", "maternal2", inplace=True)
+      
+        
+        variaveis = ["total", "bercario", "maternal1", "maternal2"]
+        dic = { "total": {"nome":"Total","qtd":0, "sexo": {"masculino": 0,"feminino": 0},"bolsa":{"sim":0,"nao":0},"alcool":{ "sim":0,"nao":0}},
+            "bercario":  {"nome":"Berçário","qtd":0, "sexo": {"masculino": 0,"feminino": 0},"bolsa":{"sim":0,"nao":0},"alcool":{"sim":0,"nao":0}},
+            "maternal1": {"nome":"Maternal 1", "qtd":0, "sexo": {"masculino": 0,"feminino": 0},"bolsa":{"sim":0,"nao":0},"alcool":{"sim":0,"nao":0}},
+            "maternal2": {"nome":"Maternal 2", "qtd":0, "sexo": {"masculino": 0,"feminino": 0},"bolsa":{"sim":0,"nao":0},"alcool":{"sim":0,"nao":0}},
+          }
+        for valor in variaveis:
+            df_aux = df.copy()
+            if valor != "total":
+                df_aux = df_aux[df_aux["turma"] == valor]
+            dic[valor]["qtd"] = len(df_aux)
+
+            
+        
+            dic[valor]["sexo"]["masculino"] = len(df_aux[df_aux["sexo"] == "Masculino"])
+            dic[valor]["sexo"]["feminino"] = len(df_aux[df_aux["sexo"] == "Feminino"])
+            
+            dic[valor]["bolsa"]["sim"] = len(df_aux[df_aux["bolsa"] == "Sim"])
+            dic[valor]["bolsa"]["nao"] = len(df_aux[df_aux["bolsa"] == "Não"])
+            
+            dic[valor]["alcool"]["sim"] = len(df_aux[df_aux["alcool"] == "Sim"])
+            dic[valor]["alcool"]["nao"] = len(df_aux[df_aux["alcool"] == "Não"])
+
+
+        return HttpResponse(JsonResponse({'status': 'OK', 'message':'Sucesso', "data":str(dic)}), content_type="application/json", status=200)
         
      
