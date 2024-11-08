0432b96388d5e44ebb8754d3fa3fb19bc65a08e9
afcarioca <afcarioca@hotmail.com>
2024-11-07 22:01:45 -0300

    Add Ambiente de teste

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,4 @@ RUN pip install --upgrade pip
 
 RUN pip install -r requirements.txt
 
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000", "--settings=api.dev"]
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000", "--settings=api.test"]
---
+++ api/test.py
@@ -0,0 +1,71 @@
+from .settings import *
+from dotenv import dotenv_values
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+config  = dotenv_values(".env")
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config["SECRET_KEY"]
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', "192.168.0.6","192.168.0.4", "192.168.0.21", "192.168.137.56", "192.168.42.138", "192.168.42.77", " 192.168.42.152", "192.168.0.19","192.168.0.21", "192.168.0.13"]
+
+
+
+
+# Application definition
+#'rest_framework_simplejwt',
+INSTALLED_APPS = [
+    "corsheaders",
+    "rest_framework",
+    "rest_framework.authtoken", 
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'example'
+]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://192.168.0.6:3000",  
+    "http://100.91.255.86:3000",  
+    "http://192.168.137.56:3000",  
+    "http://192.168.42.138:3000",  
+    "http://192.168.42.152:3000",  
+    "http://192.168.0.19:3000",  
+    "http://192.168.0.21:3000",  
+    "http://192.168.0.13:3000",  
+    "http://localhost:3000",
+]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# Note: Django modules for using databases are not support in serverless
+# environments like Vercel. You can use a database over HTTP, hosted elsewhere.
+DATABASES = {
+    'default': {
+        'ENGINE': config["PG_ENGINE_DOCKER"],
+        'NAME': config["PG_NAME_DOCKER"],
+        'USER': config["PG_USER_DOCKER"],
+        'PASSWORD': config["PG_PASSWORD_DOCKER"],
+        'HOST': config["PG_HOST_DOCKER"],
+        'PORT': config["PG_PORT_DOCKER"],
+    }
+}
+
+
+CSRF_COOKIE_HTTPONLY = True
+CSRF_COOKIE_SECURE = True
+
+
+
+
